bed1502f4cc234b14cdf3271139ddd46d95b432a
Miguel de Oliveira Dias Gonçalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-10-01 11:07:00 -0400

    First Python files

---
+++ Socket-1.py
@@ -0,0 +1,21 @@
+import socket
+
+ROUTER_1_F1_0 = '222.222.1.1'
+PORT = 80
+MTU = 1500
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Hello World")
+    s.connect((ROUTER_1_F1_0, PORT))
+    print("Hello World 2")
+    s.sendall(b'GET / HTTP/1.1\r\nHost: 222.222.1.1\r\n\r\n')
+    print("Hello World 3")
+    data = s.recv(MTU)
+    print("Hello World 4")
+    s.close()
+    print("Hello World 5")
+
+print("\nMessage received was:\n")
+print(data.decode('utf-8'))
