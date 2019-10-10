cc592b025d506b5837e2bfa27edb098ecf5a8105
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-10-10 01:37:55 +0100

    Completed program that sends and listens to OSPFv3 packets in network

--- python_files/Socket-IPv6.py
+++ python_files/Socket-IPv6.py
@@ -1,40 +1,34 @@
 import socket
-import os
+import struct
 
 '''
-Short program to experiment on sending and receiving OSPF packets over a network
+Short program to experiment on sending and receiving OSPF multicast packets over a network
 This version is dedicated to IPv6
 This program must be run as superuser
 '''
 
-ROUTER_INT_IP = 'fe80::c001:18ff:fe34:10'
-HOST_INT_IP = 'fe80::20c:29ff:feff:6bf0'
-HOST_INTERFACE_NAME = "ens33"
-MTU = 1500  # Maximum transmission unit - In bytes
-PING_SUCCESSFUL = 0  # Ping command returns 0 if successful
+ALL_OSPF_ROUTERS = 'ff02::5'
+MTU = 65565  # Maximum transmission unit - In bytes
 OSPF_PROTOCOL_NUMBER = 89
 DATA_TO_SEND = b'11111111111111'
 PACKETS_TO_WAIT = 5  # Program will wait for 5 packets
-MAX_PORT = 65565
 PORT = 0  # Has no effect and should be 0 - Still must be included
+MULTICAST_STRING_FORMAT = "=I"  # Required when joining multicast groups
 
 
-#  Test connection with router
-if os.system("ping6 -c 1 -I " + HOST_INTERFACE_NAME + " " + ROUTER_INT_IP) != PING_SUCCESSFUL:
-    print("Router could not be reached - Exiting...")
-    exit()
-else:
-    print("Router successfully reached")
-
-#  Send a packet to the router
+#  Send a packet to the router - Default TTL is 1 and it is what is required, so it remains unchanged
 s = socket.socket(socket.AF_INET6, socket.SOCK_RAW, OSPF_PROTOCOL_NUMBER)
-#  s.bind((HOST_INT_IP, 0))
-#  s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)  # Not used because the standard IP header will be kept
-s.sendto(DATA_TO_SEND, (ROUTER_INT_IP, PORT))
+s.sendto(DATA_TO_SEND, (ALL_OSPF_ROUTERS, PORT))
+
+#  Join a multicast group
+group_address_bits = socket.inet_pton(socket.AF_INET6, ALL_OSPF_ROUTERS)
+membership_parameters = group_address_bits + struct.pack(MULTICAST_STRING_FORMAT, socket.INADDR_ANY)
+s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, membership_parameters)
 
 #  Listen to packets from the network
-packets_listened = 0
-while packets_listened < PACKETS_TO_WAIT:
-    print(s.recv(MTU))
-    print(s.recvfrom(MAX_PORT))
-    packets_listened += 1
+packets_captured = 0
+while packets_captured < PACKETS_TO_WAIT:
+    print("Capturing packet", packets_captured + 1, "...")
+    print(s.recvfrom(MTU))
+    packets_captured += 1
+    print("Packet", packets_captured, "captured")
--- python_files/test_ping.py
+++ python_files/test_ping.py
@@ -7,17 +7,27 @@ This set of tests must be run as superuser
 '''
 
 PING_SUCCESSFUL = 0  # Ping command returns 0 if successful
-ROUTER_INT_IP = '222.222.1.1'
-HOST_INT_IP = '222.222.1.2'
+HOST_INTERFACE_NAME = "ens33"
+
+ROUTER_INT_IPV4 = '222.222.1.1'
+HOST_INT_IPV4 = '222.222.1.2'
+ROUTER_INT_IPV6 = '2001:db8:cafe:1::1'
+HOST_INT_IPV6 = '2001:db8:cafe:1::2'
 
 
 class PingTest(unittest.TestCase):
 
     def test_host_ping_ipv4(self):
-        self.assertEqual(PING_SUCCESSFUL, os.system("ping -c 1 " + HOST_INT_IP))
+        self.assertEqual(PING_SUCCESSFUL, os.system("ping -c 1 " + HOST_INT_IPV4))
 
     def test_router_ping_ipv4(self):
-        self.assertEqual(PING_SUCCESSFUL, os.system("ping -c 1 " + ROUTER_INT_IP))
+        self.assertEqual(PING_SUCCESSFUL, os.system("ping -c 1 " + ROUTER_INT_IPV4))
+
+    def test_host_ping_ipv6(self):
+        self.assertEqual(PING_SUCCESSFUL, os.system("ping6 -c 1 -I " + HOST_INTERFACE_NAME + " " + HOST_INT_IPV6))
+
+    def test_router_ping_ipv6(self):
+        self.assertEqual(PING_SUCCESSFUL, os.system("ping6 -c 1 -I " + HOST_INTERFACE_NAME + " " + ROUTER_INT_IPV6))
 
 
 if __name__ == '__main__':
