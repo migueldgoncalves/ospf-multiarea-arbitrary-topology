a98e0290365928948aa5db880bd82fe8fcec5e9c
migueldgoncalves <miguel.d.goncalves@tecnico.ulisboa.pt>
2019-10-25 04:58:56 +0100

    Sockets with multithreading implemented and tested

--- src/general/socket_python.py
+++ src/general/socket_python.py
@@ -5,54 +5,131 @@ import struct
 This class performs the socket operations in the router
 '''
 
-ALL_OSPF_ROUTERS = '224.0.0.5'
-ALL_DESIGNATED_ROUTERS = '224.0.0.6'
+ALL_OSPF_ROUTERS_IPV4 = '224.0.0.5'
+ALL_DESIGNATED_ROUTERS_IPV4 = '224.0.0.6'
+ALL_OSPF_ROUTERS_IPV6 = 'ff02::5'
+ALL_DESIGNATED_ROUTERS_IPV6 = 'ff02::6'
+
 MTU = 65565  # Maximum transmission unit - In bytes
 OSPF_PROTOCOL_NUMBER = 89
-PACKETS_TO_WAIT = 5  # Program will wait for 5 packets
 PORT = 0  # Has no effect and should be 0 - Still must be included
-MULTICAST_STRING_FORMAT = "4sL"  # Required when joining multicast groups
+MULTICAST_STRING_FORMAT_IPV4 = "4sL"  # Required when joining multicast groups (4s - 4 letter string; L - Signed long)
+MULTICAST_STRING_FORMAT_IPV6 = "=I"  # Required when joining multicast groups (= - Native byte order; I - Unsigned int)
 ENCODING = "UTF-8"
 
 
 class Socket:
 
-    shutdown_flag = 0  # Signals the socket thread to terminate
-    is_dr = False  # If router is a DR router
+    is_dr = False  # If router is a DR router - Only needed for testing purposes
 
-    #  Will listen to packets in the network until signaled to stop
-    def receive_ipv4(self, buffer, interface, accept_self_packets, is_dr):
+    #  Listen to IPv4 packets in the network until signaled to stop
+    def receive_ipv4(self, pipeline, shutdown, interface, accept_self_packets, is_dr):
+        if pipeline is None:
+            raise ValueError("No pipeline provided")
+        if shutdown is None:
+            raise ValueError("No shutdown event provided")
+        if interface is None:
+            raise ValueError("No interface to bind provided")
+        if interface.strip() == '':
+            raise ValueError("Empty interface to bind provided")
+
+        #  Create socket and binds it to interface
         self.is_dr = is_dr
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, OSPF_PROTOCOL_NUMBER)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(interface + '\0').encode(ENCODING))
 
         #  Join multicast group(s)
-        group_address_bits = socket.inet_aton(ALL_OSPF_ROUTERS)
-        membership_parameters = struct.pack(MULTICAST_STRING_FORMAT, group_address_bits, socket.INADDR_ANY)
+        group_address_bits = socket.inet_aton(ALL_OSPF_ROUTERS_IPV4)
+        membership_parameters = struct.pack(MULTICAST_STRING_FORMAT_IPV4, group_address_bits, socket.INADDR_ANY)
         s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership_parameters)
-
-        if is_dr:
-            group_address_bits = socket.inet_aton(ALL_DESIGNATED_ROUTERS)
-            membership_parameters = struct.pack(MULTICAST_STRING_FORMAT, group_address_bits, socket.INADDR_ANY)
+        if self.is_dr:
+            group_address_bits = socket.inet_aton(ALL_DESIGNATED_ROUTERS_IPV4)
+            membership_parameters = struct.pack(MULTICAST_STRING_FORMAT_IPV4, group_address_bits, socket.INADDR_ANY)
             s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership_parameters)
 
         #  Listen to packets from the network
-        while not self.shutdown_flag:
+        while not shutdown.is_set():
             data = s.recvfrom(MTU)
-            if data[1][0] != socket.gethostbyname(socket.gethostname()):
-                buffer.append(data)
+            #  If packet is not from itself OR if packets from itself are allowed
+            if (data[1][0] != socket.gethostbyname(socket.gethostname())) | accept_self_packets:
+                pipeline.put(data[0])
         s.close()
-        return ''
 
-    #  Will listen to packets in the network until signaled to stop
-    def receive_ipv6(self, buffer, interface, accept_self_packets, is_dr):
-        return 0
+    #  Listen to IPv6 to packets in the network until signaled to stop
+    def receive_ipv6(self, pipeline, shutdown, interface, accept_self_packets, is_dr):
+        if pipeline is None:
+            raise ValueError("No pipeline provided")
+        if shutdown is None:
+            raise ValueError("No shutdown event provided")
+        if interface is None:
+            raise ValueError("No interface to bind provided")
+        if interface.strip() == '':
+            raise ValueError("Empty interface to bind provided")
 
-    def send_ipv4(self, packet, destination_address, source_interface):
-        return 0
+        #  Create socket and binds it to interface
+        self.is_dr = is_dr
+        s = socket.socket(socket.AF_INET6, socket.SOCK_RAW, OSPF_PROTOCOL_NUMBER)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(interface + '\0').encode(ENCODING))
 
-    def send_ipv6(self, packet, destination_address, source_interface):
-        return 0
+        #  Join multicast group(s)
+        group_address_bits = socket.inet_pton(socket.AF_INET6, ALL_OSPF_ROUTERS_IPV6)
+        membership_parameters = group_address_bits + struct.pack(MULTICAST_STRING_FORMAT_IPV6, socket.INADDR_ANY)
+        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, membership_parameters)
+        if self.is_dr:
+            group_address_bits = socket.inet_pton(socket.AF_INET6, ALL_DESIGNATED_ROUTERS_IPV6)
+            membership_parameters = group_address_bits + struct.pack(MULTICAST_STRING_FORMAT_IPV6, socket.INADDR_ANY)
+            s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, membership_parameters)
 
-    def shutdown(self):
-        self.shutdown_flag = 1
+        #  Listen to packets from the network
+        while not shutdown.is_set():
+            data = s.recvfrom(MTU)
+            #  If packet is not from itself OR if packets from itself are allowed
+            if (data[1][0].split()[0] != socket.getaddrinfo(socket.gethostname(), PORT,
+                                                            socket.AF_INET6)[0][4][0]) | accept_self_packets:
+                pipeline.put(data[0])
+        s.close()
+
+    def send_ipv4(self, packet, destination_address, interface):
+        if packet is None:
+            raise ValueError("No data to send provided")
+        if packet.strip() == '':
+            raise ValueError("Empty data to send provided")
+        if destination_address is None:
+            raise ValueError("No destination address provided")
+        if destination_address.strip() == '':
+            raise ValueError("Empty destination address provided")
+        if interface is None:
+            raise ValueError("No interface to bind provided")
+        if interface.strip() == '':
+            raise ValueError("Empty interface to bind provided")
+
+        #  Create socket and bind it to interface - Default TTL is 1 and it is what is required, so it remains unchanged
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, OSPF_PROTOCOL_NUMBER)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(interface + '\0').encode(ENCODING))
+
+        #  Send packet
+        s.sendto(packet, (destination_address, PORT))
+        s.close()
+
+    def send_ipv6(self, packet, destination_address, interface):
+        if packet is None:
+            raise ValueError("No data to send provided")
+        if packet.strip() == '':
+            raise ValueError("Empty data to send provided")
+        if destination_address is None:
+            raise ValueError("No destination address provided")
+        if destination_address.strip() == '':
+            raise ValueError("Empty destination address provided")
+        if interface is None:
+            raise ValueError("No interface to bind provided")
+        if interface.strip() == '':
+            raise ValueError("Empty interface to bind provided")
+
+        #  Create socket and bind it to interface - Default TTL is 1 and it is what is required, so it remains unchanged
+        s = socket.socket(socket.AF_INET6, socket.SOCK_RAW, OSPF_PROTOCOL_NUMBER)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(interface + '\0').encode(ENCODING))
+
+        #  Send packet
+        s.sendto(packet, (destination_address, PORT))
+        s.close()
+
--- src/test/test_socket.py
+++ src/test/test_socket.py
@@ -1,110 +1,208 @@
 import unittest
 import timeout_decorator
 import threading
+import queue
 
 import general.socket_python as socket_python
 
-TIMEOUT_SECONDS = 15
+TIMEOUT_SECONDS = 30
 DATA_TO_SEND = b'11111111111111'
-OSPF_ALL_PACKETS = "224.0.0.5"
+OSPF_ALL_PACKETS_IPV4 = "224.0.0.5"
+OSPF_ALL_PACKETS_IPV6 = "ff02::5"
 SOURCE_INTERFACE = "ens33"
 
 
+#  Full successful run - 40-80 s
 class SocketTest(unittest.TestCase):
+    pipeline = None
+    shutdown = None
+    socket = None
 
+    def setUp(self):
+        self.pipeline = queue.Queue()
+        self.shutdown = threading.Event()
+        self.socket = socket_python.Socket()
+
+    #  Successful run - 10-20 s
     @timeout_decorator.timeout(TIMEOUT_SECONDS)
-    def test_receive_data_successful(self):
-        #  For IPv4
-        buffer = []
+    def test_receive_data_successful_ipv4(self):
         accept_self_packets = False
         is_dr = False
-        socket = socket_python.Socket()
-        thread = threading.Thread(target=socket.receive_ipv4, args=(buffer, SOURCE_INTERFACE, accept_self_packets,
-                                                                    is_dr))
+        thread = threading.Thread(target=self.socket.receive_ipv4, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
         thread.start()
-        while len(buffer) == 0:
-            if len(buffer) > 0:
-                break
-        socket.shutdown()
+        while self.pipeline.qsize() == 0:
+            pass
+        self.shutdown.set()  # Signals the thread to shutdown
         thread.join()
 
-        #  For IPv6
-        buffer = []
-        socket = socket_python.Socket()
-        thread = threading.Thread(target=socket.receive_ipv6, args=(buffer, SOURCE_INTERFACE, accept_self_packets,
-                                                                    is_dr))
-        thread.start()
-        while len(buffer) == 0:
-            if len(buffer) > 0:
-                break
-        socket.shutdown()
-        thread.join()
-
-    def test_is_dr(self):
-        buffer = []
+    #  Successful run - 10-20 s
+    @timeout_decorator.timeout(TIMEOUT_SECONDS)
+    def test_receive_data_successful_ipv6(self):
         accept_self_packets = False
         is_dr = False
-        socket = socket_python.Socket()
-        thread = threading.Thread(target=socket.receive_ipv4, args=(buffer, SOURCE_INTERFACE, accept_self_packets,
-                                                                    is_dr))
+        thread = threading.Thread(target=self.socket.receive_ipv6, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
         thread.start()
-        socket.shutdown()
+        while self.pipeline.qsize() == 0:
+            pass
+        self.shutdown.set()
         thread.join()
-        self.assertFalse(socket.is_dr)
+
+    #  Successful run - Instant
+    @timeout_decorator.timeout(TIMEOUT_SECONDS)
+    def test_is_dr_ipv4(self):
+        accept_self_packets = False
+        is_dr = False
+        thread = threading.Thread(target=self.socket.receive_ipv4, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
+        thread.start()
+        self.shutdown.set()
+        thread.join()
+        self.assertFalse(self.socket.is_dr)
 
         is_dr = True
-        thread = threading.Thread(target=socket.receive_ipv4, args=(buffer, SOURCE_INTERFACE, accept_self_packets,
-                                                                    is_dr))
+        thread = threading.Thread(target=self.socket.receive_ipv4, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
         thread.start()
-        socket.shutdown()
+        self.shutdown.set()
         thread.join()
-        self.assertTrue(socket.is_dr)
+        self.assertTrue(self.socket.is_dr)
 
-    def test_receive_no_parameters(self):
-        buffer = []
+    #  Successful run - Instant
+    @timeout_decorator.timeout(TIMEOUT_SECONDS)
+    def test_is_dr_ipv6(self):
         accept_self_packets = False
         is_dr = False
-        self.assertRaises(ValueError,
-                          socket_python.Socket.receive_ipv4(socket_python.Socket(), '', buffer, accept_self_packets,
-                                                            is_dr))
-        self.assertRaises(ValueError,
-                          socket_python.Socket.receive_ipv6(socket_python.Socket(), '', buffer, accept_self_packets,
-                                                            is_dr))
-        self.assertRaises(ValueError,
-                          socket_python.Socket.receive_ipv4(socket_python.Socket(), SOURCE_INTERFACE, None,
-                                                            accept_self_packets, is_dr))
-        self.assertRaises(ValueError,
-                          socket_python.Socket.receive_ipv6(socket_python.Socket(), SOURCE_INTERFACE, None,
-                                                            accept_self_packets, is_dr))
+        thread = threading.Thread(target=self.socket.receive_ipv6, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
+        thread.start()
+        self.shutdown.set()
+        thread.join()
+        self.assertFalse(self.socket.is_dr)
 
-    def test_send_data_successful(self):
-        #  For IPv4
-        buffer = []
+        is_dr = True
+        thread = threading.Thread(target=self.socket.receive_ipv6, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
+        thread.start()
+        self.shutdown.set()
+        thread.join()
+        self.assertTrue(self.socket.is_dr)
+
+    #  Successful run - Instant
+    def test_receive_invalid_parameters_ipv4(self):
+        accept_self_packets = False
+        is_dr = False
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv4(socket_python.Socket(), None, self.shutdown, SOURCE_INTERFACE,
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv4(socket_python.Socket(), self.pipeline, None, SOURCE_INTERFACE,
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv4(socket_python.Socket(), self.pipeline, self.shutdown, None,
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv4(socket_python.Socket(), self.pipeline, self.shutdown, '',
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv4(socket_python.Socket(), self.pipeline, self.shutdown, '        ',
+                                              accept_self_packets, is_dr)
+
+    #  Successful run - Instant
+    def test_receive_invalid_parameters_ipv6(self):
+        accept_self_packets = False
+        is_dr = False
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv6(socket_python.Socket(), None, self.shutdown, SOURCE_INTERFACE,
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv6(socket_python.Socket(), self.pipeline, None, SOURCE_INTERFACE,
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv6(socket_python.Socket(), self.pipeline, self.shutdown, None,
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv6(socket_python.Socket(), self.pipeline, self.shutdown, '',
+                                              accept_self_packets, is_dr)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.receive_ipv6(socket_python.Socket(), self.pipeline, self.shutdown, '        ',
+                                              accept_self_packets, is_dr)
+
+    #  Successful run - 10-20 s
+    @timeout_decorator.timeout(TIMEOUT_SECONDS)
+    def test_send_data_successful_ipv4(self):
         accept_self_packets = True
         is_dr = False
-        socket = socket_python.Socket()
-        thread = threading.Thread(target=socket.receive_ipv4, args=(buffer, SOURCE_INTERFACE, accept_self_packets,
-                                                                    is_dr))
-        thread.start()
-        socket.send_ipv4(DATA_TO_SEND, OSPF_ALL_PACKETS, SOURCE_INTERFACE)
-        self.assertTrue(len(buffer) > 0)
-        socket.shutdown()
+        thread = threading.Thread(target=self.socket.receive_ipv4, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
+        thread.start()  # Sends a packet that should be listened by the receiving socket
+        self.socket.send_ipv4(DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        while self.pipeline.qsize() == 0:
+            pass
+        self.shutdown.set()
+        thread.join()
 
-        # For IPv6
-        buffer = []
-        socket = socket_python.Socket()
-        thread = threading.Thread(target=socket.receive_ipv4, args=(buffer, SOURCE_INTERFACE, accept_self_packets,
-                                                                    is_dr))
-        thread.start()
-        socket.send_ipv6(DATA_TO_SEND, OSPF_ALL_PACKETS, SOURCE_INTERFACE)
-        self.assertTrue(len(buffer) > 0)
-        socket.shutdown()
+    #  Successful run - 10-20 s
+    @timeout_decorator.timeout(TIMEOUT_SECONDS)
+    def test_send_data_successful_ipv6(self):
+        accept_self_packets = True
+        is_dr = False
+        thread = threading.Thread(target=self.socket.receive_ipv6, args=(self.pipeline, self.shutdown, SOURCE_INTERFACE,
+                                                                         accept_self_packets, is_dr))
+        thread.start()  # Sends a packet that should be listened by the receiving socket
+        self.socket.send_ipv6(DATA_TO_SEND, OSPF_ALL_PACKETS_IPV6, SOURCE_INTERFACE)
+        while self.pipeline.qsize() == 0:
+            pass
+        self.shutdown.set()
+        thread.join()
 
-    def test_send_no_parameters(self):
-        socket = socket_python.Socket()
-        self.assertRaises(ValueError, socket.send_ipv4(None, OSPF_ALL_PACKETS, SOURCE_INTERFACE))
-        self.assertRaises(ValueError, socket.send_ipv4(DATA_TO_SEND, '', SOURCE_INTERFACE))
-        self.assertRaises(ValueError, socket.send_ipv4(DATA_TO_SEND, OSPF_ALL_PACKETS, ''))
+    #  Successful run - Instant
+    def test_send_invalid_parameters_ipv4(self):
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), None, OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), '', OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), '        ', OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), DATA_TO_SEND, None, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), DATA_TO_SEND, '', SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), DATA_TO_SEND, '        ', SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, None)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, '')
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv4(socket_python.Socket(), DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, '        ')
+
+    #  Successful run - Instant
+    def test_send_invalid_parameters_ipv6(self):
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), None, OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), '', OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), '        ', OSPF_ALL_PACKETS_IPV4, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), DATA_TO_SEND, None, SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), DATA_TO_SEND, '', SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), DATA_TO_SEND, '        ', SOURCE_INTERFACE)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, None)
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, '')
+        with self.assertRaises(ValueError):
+            socket_python.Socket.send_ipv6(socket_python.Socket(), DATA_TO_SEND, OSPF_ALL_PACKETS_IPV4, '        ')
+
+    def tearDown(self):
+        self.pipeline = None
+        self.shutdown = None
+        self.socket = None
 
 
 if __name__ == '__main__':
